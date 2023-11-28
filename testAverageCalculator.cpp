b35ac54dbe43efe76dbd9a17591ad37a7b32d697
ItxKabeerAhmad <152272069+ItxKabeerAhmad@users.noreply.github.com>
2023-11-28 22:17:06 +0500

    Add files via upload

---
+++ testAverageCalculator.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int test1,test2,test3,test4,test5;
+    cout<<"ENTER THE MARKS OF YOUR TESTS: ";
+    cin>>test1>>test2>>test3>>test4>>test5;
+    cout<<"THE AVERAGE OF YOUR MARKS IS "<<(test1+test2+test3+test4+test5)/5;
+    return 0;
+}
